e2d6f7996011114db6da9bea394b29f5a8f7e449
developer <cpapideveloper@gmail.com>
2022-06-14 22:33:59 -0700

    Add clang-format and doxygen support

--- modules/sample/include/sample.hpp
+++ modules/sample/include/sample.hpp
@@ -6,9 +6,9 @@
 extern "C" {
 #endif
 
-  int caAdd(int m, int n);
+int caAdd(int m, int n);
 
-  int caWriteString(char* buffer, size_t buffer_size, const char* string);
+int caWriteString(char* buffer, size_t buffer_size, const char* string);
 
 #if __cplusplus
 }
