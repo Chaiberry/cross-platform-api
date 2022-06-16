2a288d384def17932e44ecf83d34905dd3a7b589
developer <cpapideveloper@gmail.com>
2022-06-15 22:13:30 -0700

    fix doxygen doc generation

--- modules/sample/include/sample.hpp
+++ modules/sample/include/sample.hpp
@@ -6,6 +6,15 @@
 extern "C" {
 #endif
 
+/**
+ * \brief Adds two numbers.
+ *
+ * This function takes two numbers, adds them, and then returns the result.
+ *
+ * \param m The first number to add.
+ * \param n The second number to add.
+ * \return The sum of the two numbers.
+ */
 int caAdd(int m, int n);
 
 int caWriteString(char* buffer, size_t buffer_size, const char* string);
